eb97b3a6dab007bb6e9f421c1514e6b08f58d218
chems <chayma.kilic@lepoles.org>
2024-06-12 17:13:29 +0200

    120624

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php include __DIR__ . "/public/header.html.php" ?>
---
+++ public/header.html.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <base href="http://localhost/boutique/">
+  <title>Ma Boutique</title>
+  <link rel="stylesheet" href="public\assets\css\style.css">
+</head>
+<body>
+  <div class="container">
+    <header>
+      <div class= logo>
+        <div>
+          <img src="public\assets\img\logo.png" alt="">
+        </div>
+        <div class="logotitle">
+          <h1>Ma Boutique</h1>
+          <p>Vente en ligne. Tendance et Déco </p>
+        </div>
+      </div>
+      <div class= logotitle2>
+      <h2>Besoin d'aide?</h2>
+      <img src="public\assets\img\aide.png" alt="">
+</div>
+<div class= logotitle3>
+  <h3>Mon compte</h3>
+  <div class= two>
+    <p>article(s) dans votre panier.</p>
+</div>
+
+</div>
+
+</div>
+    </header>
